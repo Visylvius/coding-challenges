59521c27c64497218c219e9fa85e6df401b8d5f7
Loren Barrick <lorenbarrick@gmail.com>
2016-07-29 11:41:01 -0700

    completele challenges

--- greatestCommonDivisor.js
+++ greatestCommonDivisor.js
@@ -1,87 +1,37 @@
-// sort input array
-// go from greatest divisor to least divisor from greatest value and check if they are in the rest of the divisors
 function greatestCommonDivisor(array) {
   var commonDivisor = 1;
-  //to handle negative numbers
   var unmutatedArray = array.slice(0);
-  for (var i = 0; i < array.length; i++) {
-    unmutatedArray[i] = Math.abs(unmutatedArray[i]);
+  function getDivisor(number) {
+    var storage = [];
+    for (var i = number; i > 0; i--) {
+      if (number % i === 0 ) {
+        storage.push(i);
+      }
+    }
+    return storage;
   }
+
+  function getAllDivisors(array) {
+    var divisorArray = [];
+    for (var i = 0; i < array.length; i++) {
+      divisorArray[i] = getDivisor(array[i]);
+    }
+    return divisorArray;
+  }
+
   var storage = getAllDivisors(unmutatedArray);
+  var divisorsForFirstNumber = storage.shift();
 
-
-  var lowestCommonDivisorArray = storage.shift();
-  // [ 3, 1 ]
-  // [6, 3, 2, 1], [9, 3, 1]
-  for (var i = 0; i < lowestCommonDivisorArray.length; i++) {
+  for (var i = 0; i < divisorsForFirstNumber.length; i++) {
     var stillADivisor = true;
     for (var j = 0; j < storage.length; j++) {
-      if (storage[j].indexOf(lowestCommonDivisorArray[i]) !== -1 && stillADivisor
-         && commonDivisor < lowestCommonDivisorArray[i] && storage.length - 1 === j) {
-         commonDivisor = lowestCommonDivisorArray[i];
-      } else if (storage[j].indexOf(lowestCommonDivisorArray[i]) === -1) {
+      if (storage[j].indexOf(divisorsForFirstNumber[i]) !== -1 && stillADivisor
+         && commonDivisor < divisorsForFirstNumber[i] && storage.length - 1 === j) {
+         commonDivisor = divisorsForFirstNumber[i];
+      } else if (storage[j].indexOf(divisorsForFirstNumber[i]) === -1) {
         stillADivisor = false;
       }
     }
   }
-  // console.log([1,2,3,4,5].indexOf(7));
-
   return commonDivisor;
 }
-
-function getDivisor(number) {
-  var storage = [];
-  for (var i = number; i > 0; i--) {
-    if (number % i === 0 ) {
-      storage.push(i);
-    }
-  }
-  return storage;
-}
-
-function getAllDivisors(array) {
-  var divisorArray = [];
-  for (var i = 0; i < array.length; i++) {
-    divisorArray[i] = getDivisor(array[i]);
-  }
-  return divisorArray;
-}
-
-console.log(greatestCommonDivisor([150, 30, 90]) === 30);
-//Write a method that returns the greatest common divisor between an array of integers that are passed into the method
-
-
-// [3, 6, 12, 24];
-// 1
-
-// 3 = 3, 1
-// 6 = 6, 3, 2, 1
-// 12 = 12, 6, 4, 3, 2, 1
-// 24 = 24, 12, 8, 6, 4, 3, 2, 1
-
-
-// 1 = 1
-// 2 = 2, 1
-// 3 = 3, 1
-// 5 = 5, 1
-// 8 = 8, 4, 2, 1
-// 12 = 6, 4, 3, 2, 1
-
-
-
-//expected output
-//Examples:
-// [3,6,9] -> 3
-// [5,12,8] -> 1
-// [30,150,90] -> 30
-// [30, 35, 90, 150] -> 30
-// [1, 2, 3, 5, 8, 12] -> 3
-
-
-// [30, 90, 150] ->
-// divisorArray =
-//   [
-//    [30, 15, 10, 5, 3, 2, 1 ],
-//    [30, 15, 10, 6, 5, 3, 2, 1 ],
-//    [30, 25, 15, 10, 6, 5, 3, 2, 1 ]
-//   ]
--- takeCharOutOfString.js
+++ takeCharOutOfString.js
@@ -1,4 +1,4 @@
-function reverseString(str) {
+function reverse(str) {
   return str
     .split('')
     .reverse()
@@ -6,33 +6,17 @@ function reverseString(str) {
 }
 
 function reverseSpecial(word){
-  // we need a way to save the index of Q or q
-  var originalWord = word;
   var qindexes = [];
   for (var index = 0; index < word.length; index++) {
     var character = word[index];
-    // check to see if the character at i is equal to either q or Q
-    // if it is equal to q or Q save the index
     if (character === 'q') {
       qindexes.push(['q', index]);
     } else if (character === 'Q') {
       qindexes.push(['Q', index]);
     }
   }
-
-
-  // remove the Q and q from the word
   var wordWithoutQ = clear(word, 'q');
-  // reverse the qQ-less word
-  var reversedWord = reverseString(wordWithoutQ);
-
-  //compare two indexes of q and Q array
-    //if one index is less than the other
-      //call insertAt with proper parameters
-      //else call insertAT with other parameters
-
-  // console.log('final word', finalWord);
-  // then we add q's and Q's at the indexes of the proper arrays
+  var reversedWord = reverse(wordWithoutQ);
   var completeWord = insertAtX(reversedWord, qindexes);
   return completeWord;
 }
@@ -51,18 +35,6 @@ function insertAtX(word, indexes) {
   return word;
 }
 
-// function newClear(word, character) {
-//   return word
-//     .split('')
-//   for (var i = 0; i < word.length; i++) {
-//     if (word[i] === character || word[i] === character.toUpperCase()) {
-//       word[i] = '';
-//     }
-//   }
-// }
-
-// console.log(newClear('QQabcqq QQabcqq', 'q'));
-
 function clear(word, character) {
   return word
     .split('')
@@ -78,27 +50,10 @@ function clear(word, character) {
     .join('');
 }
 
-function getFinalWord(word) {
+function reverseString(word) {
   var seperatedWord = word.split(' ');
   for (var i = 0; i < seperatedWord.length; i++) {
     seperatedWord[i] = reverseSpecial(seperatedWord[i]);
   }
   return seperatedWord.join(' ');
 }
-/* AMAZING VANILLA JAVASCRIPT TEST FRAMEWORK */
-
-// console.log(reverseString('hello') == 'olleh');
-//console.log(clear('abqcdQ', 'q') === 'abcd')
-// console.log(insertAt('laf', 1, 'o') === 'loaf');
-console.log(getFinalWord('h2lQkq wroqq') === 'kl2Qhq orwqq');
-
-['h2lQkq', 'wroqq'];
-
-//output
-//h2lQkq wroqq => kl2Qhq orwqq
-
-
-
-
-//Write a method or collection of methods that will reverse the letters of each word in a string while leaving all instances of ‘Q’ or ‘q’ in place (stay at the current index). Each word is defined as a series of characters separated by a single whitespace(‘ ‘). Example: h2lQkq wroqq => kl2Qhq orwqq *
-//Example output: h2lQkq wroqq => kl2Qhq orwqq
